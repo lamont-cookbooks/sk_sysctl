d447842f19a31eeabbe837c613adc4b147a26c88
Lamont Granquist <lamont@opscode.com>
2012-06-06 16:05:04 -0700

    fix overcommit to actually overcommit like a mofo

--- recipes/vm.rb
+++ recipes/vm.rb
@@ -5,7 +5,7 @@ sysctl "vm.dirty_ratio = 60"
 sysctl "vm.dirty_background_ratio = 2"
 
 # overcommit like a mofo
-sysctl "vm.overcommit_memory = 2"
+sysctl "vm.overcommit_memory = 1"
 
 # Keep at least 64MB of free RAM space available
 sysctl "vm.min_free_kbytes = 65536"
