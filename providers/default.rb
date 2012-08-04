e97d76411ec7158090817972eb5287bbcbdff3dd
Lamont Granquist <lamont@scriptkiddie.org>
2012-08-03 20:11:08 -0700

    idempotency fix

--- providers/default.rb
+++ providers/default.rb
@@ -5,7 +5,9 @@ action :set do
 
   node.default['sysctl'] ||= {}
   node.default['sysctl'][key] = value
-  execute "sysctl -w #{key}=#{value}"
+  execute "sysctl -w #{key}=#{value}" do
+    not_if { `sysctl #{key}`.chomp == "#{key} = #{value}" }
+  end
   new_resource.notifies :create, new_resource.resources(:template => "/etc/sysctl.conf")
 end
 
