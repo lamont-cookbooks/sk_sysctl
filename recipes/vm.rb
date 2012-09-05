d0008386c0e2261ba92eadea06cd3d30eb25f7d2
Lamont Granquist <lamont@opscode.com>
2012-09-04 17:23:42 -0700

    fix some vm tweaks that were inappropriate for small linode nodes

--- recipes/vm.rb
+++ recipes/vm.rb
@@ -1,12 +1,14 @@
 
-# Do less swapping
-sysctl "vm.swappiness = 10"
-sysctl "vm.dirty_ratio = 60"
+# only swap when forced to
+sysctl "vm.swappiness = 0"
+# when a file buffers hit 10% of total mem flush them (more aggressive than default)
+sysctl "vm.dirty_ratio = 10"
+# when 2% of memory is dirty run pdflush (more aggressive, more flushes)
 sysctl "vm.dirty_background_ratio = 2"
 
 # overcommit like a mofo
 sysctl "vm.overcommit_memory = 1"
 
 # Keep at least 64MB of free RAM space available
-sysctl "vm.min_free_kbytes = 65536"
+#sysctl "vm.min_free_kbytes = 65536"
 
