842762bbd5fb12567aeffa94a0c6f15013693c65
Lamont Granquist <lamont@opscode.com>
2012-10-10 15:42:21 -0700

    fixing sysctl cookbook to work with multi-valued sysctls

--- providers/default.rb
+++ providers/default.rb
@@ -1,12 +1,13 @@
 
 action :set do
-  new_resource.expr =~ /(\S+)\s*=\s*(\S+)/
+  new_resource.expr =~ /(\S+)\s*=\s*(.+)/
   key, value = $1, $2
 
   node.default['sysctl'] ||= {}
   node.default['sysctl'][key] = value
-  execute "sysctl -w #{key}=#{value}" do
-    not_if { `sysctl #{key}`.chomp == "#{key} = #{value}" }
+  re = /^#{key} = #{value.gsub(/\s+/, "\\s+")}$/
+  execute "sysctl -w '#{key}=#{value}'" do
+    not_if { re.match(`sysctl #{key}`.chomp) }
   end
   new_resource.notifies :create, new_resource.resources(:template => "/etc/sysctl.conf")
 end
