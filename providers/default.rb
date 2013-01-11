b6b4d781cd2636d64447ed6a212bdf8fe7dd6006
Lamont Granquist <lamont@opscode.com>
2013-01-10 17:33:55 -0800

    bit more idempotent

--- providers/default.rb
+++ providers/default.rb
@@ -6,9 +6,9 @@ action :set do
   node.default['sysctl'] ||= {}
   node.default['sysctl'][key] = value
   re = /^#{key} = #{value.gsub(/\s+/, "\\s+")}$/
-  execute "sysctl -w '#{key}=#{value}'" do
-    not_if { re.match(`sysctl #{key}`.chomp) }
+  unless re.match(`sysctl #{key}`.chomp)
+    execute "sysctl -w '#{key}=#{value}'"
+    new_resource.notifies :create, new_resource.resources(:template => "/etc/sysctl.conf")
   end
-  new_resource.notifies :create, new_resource.resources(:template => "/etc/sysctl.conf")
 end
 
