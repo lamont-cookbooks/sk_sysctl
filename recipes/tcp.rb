2c05cf3c4564b5803fd7d857a516fb949ee85570
Lamont Granquist <lamont@opscode.com>
2013-01-28 20:34:37 -0800

    dropping the default value down quite a bit

--- recipes/tcp.rb
+++ recipes/tcp.rb
@@ -2,8 +2,8 @@
 # bigass windows
 sysctl "net.core.wmem_max=12582912"
 sysctl "net.core.rmem_max=12582912"
-sysctl "net.ipv4.tcp_rmem=10240 12582912 12582912"
-sysctl "net.ipv4.tcp_wmem=10240 12582912 12582912"
+sysctl "net.ipv4.tcp_rmem=10240 1048576 12582912"
+sysctl "net.ipv4.tcp_wmem=10240 1048576 12582912"
 
 # performance
 sysctl "net.ipv4.tcp_window_scaling = 1"
