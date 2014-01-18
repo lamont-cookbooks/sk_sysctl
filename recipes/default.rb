2fd7a3ae313ce8671c882488ac684a0672657489
Lamont Granquist <lamont@scriptkiddie.org>
2014-01-18 10:32:54 -0800

    making proper library cookbook

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,7 +1,1 @@
-
-include_recipe "sysctl::vm"
-include_recipe "sysctl::tcp"
-include_recipe "sysctl::shm"
-include_recipe "sysctl::behavior"
-include_recipe "sysctl::security"
-
+# intentionally left blank
--- recipes/router.rb
+++
@@ -1,5 +0,0 @@
-
-sysctl "net.ipv4.ip_forward = 1" do
-  compiletime true
-end
-
